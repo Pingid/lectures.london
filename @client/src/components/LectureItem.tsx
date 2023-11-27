bee659b95488f7285bbff16547e532d7af6d0876
Dan Beaven <dm.beaven@gmail.com>
2023-11-27 15:34:28 +0100

    Add arrow to lecture list item on desktop select

--- @client/src/components/LectureItem.tsx
+++ @client/src/components/LectureItem.tsx
@@ -1,6 +1,7 @@
 import dayjs from 'dayjs'
 import { cn } from 'mcn'
 
+import { ArrowLeftIcon } from './Icons'
 import { router } from '../router'
 import { slugit } from '../util'
 
@@ -18,16 +19,19 @@ export const LectureListItem = (p: { lecture: Lecture; disableTime?: boolean }) 
       <div class={cn('pb-2 sm:pr-4 sm:px-3 sm:pt-1')}>
         {!p.disableTime && <p class="sm:text-right">{dayjs(p.lecture.time_start).format('HH:mm')}</p>}
       </div>
-      <div class={cn('w-full border-b pb-3 sm:pb-2 sm:pt-1')}>
+      <div class={cn('w-full border-b transition-all pb-3 sm:pt-1', [!!m(), 'sm:pb-8 border-b-2', 'sm:pb-2'])}>
         <a
           {...router.link({ to: '/:host/:lecture', params, state: p.lecture })}
           class={cn('hover:underline underline-offset-4', [
             !!m(),
-            'underline',
+            'underline [text-decoration-color:var(--fg2-color)]',
             'hover:[text-decoration-color:var(--fg2-color)]',
           ])}
         >
-          <p class="cursor-pointer">{p.lecture.title}</p>
+          <p class="cursor-pointer">
+            <ArrowLeftIcon class={cn('rotate-180 inline transition-all relative', [!!m(), 'w-5 h-5 mr-1', 'w-0'])} />{' '}
+            {p.lecture.title}
+          </p>
         </a>
       </div>
     </article>
